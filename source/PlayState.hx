6c73cc4b1d46a23ac5858732daf14490837de730
Arandomguylool <102329658+Arandomguylool@users.noreply.github.com>
2022-03-31 19:24:06 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5109,10 +5109,10 @@ class PlayState extends MusicBeatState
 			//Dodge code, yes it's bad but oh well. -Haz
 			//var dodgeButton = controls.ACCEPT; //I have no idea how to add custom controls so fuck it. -Haz
 
-			if(controls.ACCEPT || _pad.buttonA.justPressed)
+			if(controls.ACCEPT)
 				trace('butttonpressed');
 
-			if(controls.ACCEPT || _pad.buttonA.justPressed && !bfDodging && bfCanDodge){
+			if(controls.ACCEPT && !bfDodging && bfCanDodge){
 				trace('DODGE START!');
 				bfDodging = true;
 				bfCanDodge = false;
@@ -5149,10 +5149,10 @@ class PlayState extends MusicBeatState
 			//Dodge code, yes it's bad but oh well. -Haz
 			//var dodgeButton = controls.ACCEPT; //I have no idea how to add custom controls so fuck it. -Haz
 			//Haha Copy-paste LOL (although modified a bit)
-			if(controls.ACCEPT || _pad.buttonA.justPressed)
+			if(controls.ACCEPT)
 				trace('butttonpressed');
 
-			if(controls.ACCEPT || _pad.buttonA.justPressed && !bfDodging && bfCanDodge){
+			if(controls.ACCEPT && !bfDodging && bfCanDodge){
 				trace('DODGE START!');
 				bfDodging = true;
 				bfCanDodge = false;
