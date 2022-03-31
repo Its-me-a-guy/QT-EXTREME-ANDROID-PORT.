725174dd1239182379b390da332a3026771fdf4b
Arandomguylool <102329658+Arandomguylool@users.noreply.github.com>
2022-03-31 17:29:35 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2072,11 +2072,11 @@ class PlayState extends MusicBeatState
 
                 #if android
 	        addAndroidControls();
-					controls.setVirtualpad(androidc._virtualpad, FULL, NONE);
-					_pad = new FlxVirtualpad(NONE, A);
-					_pad.alpha = 0.75;
-					_pad.cameras = [camHUD];
-					this.add(_pad);
+		controls.setVirtualpad(androidc._virtualpad, FULL, NONE);
+		_pad = new FlxVirtualpad(NONE, A);
+		_pad.alpha = 0.75;
+		_pad.cameras = [camHUD];
+		this.add(_pad);
                 #end
 
 		// if (SONG.song == 'South')
