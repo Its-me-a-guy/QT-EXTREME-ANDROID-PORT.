e7db347010eacdd1a230ed508d49d562512efc72
Arandomguylool <102329658+Arandomguylool@users.noreply.github.com>
2022-04-01 14:22:07 -0300

    Update DialogueBox.hx

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -437,7 +437,19 @@ class DialogueBox extends FlxSpriteGroup
 			dialogueStarted = true;
 		}
 
-		if (FlxG.keys.justPressed.ANY  && dialogueStarted == true)
+		#if android
+                var justTouched:Bool = false;
+
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+			{
+				justTouched = true;
+			}
+		}
+		#end
+
+		if (FlxG.keys.justPressed.ANY #if mobile || justTouched #end && dialogueStarted == true)
 		{
 			remove(dialogue);
 				
